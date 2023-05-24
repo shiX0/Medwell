d1ea8f6b4eb7fce6fbde9f86d48cc30eca7f6a46
shishir <shisirsharma22@gmail.com>
2023-05-24 18:25:14 +0545

    Added Login screen and removed generated main code

---
+++ lib/Screens/loginScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class loginScreen extends StatefulWidget {
+  const loginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
