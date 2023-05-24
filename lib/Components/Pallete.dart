90d40011de880d23c9b164731fb671983ddd613f
shishir <shisirsharma22@gmail.com>
2023-05-24 18:15:20 +0545

    colour pallete added

---
+++ lib/Components/Pallete.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class Pallete{
+  static const Color backgroundColor = Colors.white;
+  static const Color primary = Color(0xFFFFBDBD);
+  static const Color highlight =Color(0xFF7173A7);
+  static const Color accent = Color(0xFFEFF9F8);
+  static const Color btnAccent = Color(0xFFE6E6E6);
+}
