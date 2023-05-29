a9d0c26c79909dcf1fbf451acb3c9de3f8ef7f4d
puja335 <shahipuja335@gmail.com>
2023-05-29 23:28:19 +0545

    adding textEditingcontroller

--- lib/Screens/confirmation-mail.dart
+++ lib/Screens/confirmation-mail.dart
@@ -10,6 +10,13 @@ class EmailVerify extends StatefulWidget {
 class _EmailVerifyState extends State<EmailVerify> {
   @override
   Widget build(BuildContext context) {
+    final TextEditingController _fieldOne = TextEditingController();
+    final TextEditingController _fieldTwo = TextEditingController();
+    final TextEditingController _fieldThree = TextEditingController();
+    final TextEditingController _fieldFour = TextEditingController();
+    final TextEditingController _fieldFive = TextEditingController();
+    final TextEditingController _fieldSix = TextEditingController();
+
     return const Placeholder();
   }
 }
