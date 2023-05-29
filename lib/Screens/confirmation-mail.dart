009acb317beca316ba2b5481213cfd2db4c81d70
puja335 <shahipuja335@gmail.com>
2023-05-29 23:21:41 +0545

    started verify mail

---
+++ lib/Screens/confirmation-mail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmailVerify extends StatefulWidget {
+  const EmailVerify({Key? key}) : super(key: key);
+
+  @override
+  State<EmailVerify> createState() => _EmailVerifyState();
+}
+
+class _EmailVerifyState extends State<EmailVerify> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
