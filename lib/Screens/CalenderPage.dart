d7491e6faacf397604ac5df712396eb58e24cb79
Palden <kazamanemesis1811@gmail.com>
2023-06-06 11:48:28 +0545

    Added Calender Page

---
+++ lib/Screens/CalenderPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalenderPage extends StatefulWidget {
+  const CalenderPage({Key? key}) : super(key: key);
+
+  @override
+  State<CalenderPage> createState() => _CalenderPageState();
+}
+
+class _CalenderPageState extends State<CalenderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
