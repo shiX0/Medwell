6a08793c07bb675df4995a6e482c00ed4010a87e
Palden <kazamanemesis1811@gmail.com>
2023-06-09 16:33:56 +0545

    calender page done

--- lib/Screens/CalenderPage.dart
+++ lib/Screens/CalenderPage.dart
@@ -1,53 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class CalenderPage extends StatefulWidget {
-  const CalenderPage({Key? key}) : super(key: key);
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({Key? key}) : super(key: key);
 
   @override
-  State<CalenderPage> createState() => _CalenderPageState();
+  _CalendarPageState createState() => _CalendarPageState();
 }
 
-class _CalenderPageState extends State<CalenderPage> {
-  DateTime today=DateTime.now();
-  void _onDaySelected(DateTime day,DateTime focusedDay){
+class _CalendarPageState extends State<CalendarPage> {
+  DateTime _selectedDay = DateTime.now();
+
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
     setState(() {
-      today=day;
+      _selectedDay = day;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-
-  Widget content(){
-    return Column(
-      children: [
-        Column(
-          children: [
-            Text("123"),
-            Container(
-              child: TableCalendar(
-                focusedDay:today,
-                firstDay:DateTime.utc(1975,1,1),
-                lastDay:DateTime.utc(2030,12,31),
-                onDaySelected:_onDaySelected,
-                locale:"en_US",
-                rowHeight:43,
-                headerStyle:
-                    HeaderStyle(formatButtonVisible:false,titleCentered:true),
-                availableGestures:AvailableGestures.all,
-                selectedDayPredicate:(day)=>isSameDay(day,today),
-
-
-              ),
-            )
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendar Page'),
+      ),
+      body: Center(
+        child: TableCalendar(
+          focusedDay: _selectedDay,
+          firstDay: DateTime.utc(2022, 1, 1),
+          lastDay: DateTime.utc(2024, 12, 31),
+          onDaySelected: _onDaySelected,
         ),
-      ],
+      ),
     );
   }
 }
-
 
 
