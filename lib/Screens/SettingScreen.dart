bbfbc23f06127c2eaf3ca111eebaf7a3d73eb9ab
puja335 <shahipuja335@gmail.com>
2023-06-12 20:09:09 +0545

    :tada: started working on setting page

---
+++ lib/Screens/SettingScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Setting extends StatelessWidget {
+  const Setting({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
