245931d19e9fcefc968a7e4a238f98752368ff88
puja335 <shahipuja335@gmail.com>
2023-06-13 20:15:09 +0545

    initial commit of setting page

--- lib/Components/ProfileButton.dart
+++ lib/Components/ProfileButton.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:medwell/Components/Palette.dart';
 
 import 'Pallete.dart';
 
--- lib/Screens/SettingScreen.dart
+++ lib/Screens/SettingScreen.dart
@@ -1,11 +1,1 @@
 import 'package:flutter/material.dart';
-
-class Setting extends StatelessWidget {
-  const Setting({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
-
