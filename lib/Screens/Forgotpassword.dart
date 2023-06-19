46963af04e8a85e76edaacaeee2bc79932f7f607
rojalronaldo <rojalronaldo77@gmail.com>
2023-06-19 11:07:20 +0545

    Centered the widgets

--- lib/Screens/Forgotpassword.dart
+++ lib/Screens/Forgotpassword.dart
@@ -7,32 +7,34 @@ class ForgotPasswordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(16),
-              child: Image.asset(
-                'assets/images/Password_Monochromatic 1.png',
-                height: 200,
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(16),
+                child: Image.asset(
+                  'assets/images/Password_Monochromatic 1.png',
+                  height: 200,
+                ),
               ),
-            ),
-            Text(
-              'Reset Password',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+              Text(
+                'Reset Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Text('Use a strong password.'),
-            SizedBox(height: 16),
-            CustomTextInputField(hintText: "New password", keyboardType: TextInputType.text,),
-            SizedBox(height: 16),
-            CustomTextInputField(hintText: "Confirm password", keyboardType: TextInputType.text),
+              SizedBox(height: 16),
+              Text('Use a strong password.'),
+              SizedBox(height: 16),
+              CustomTextInputField(hintText: "New password", keyboardType: TextInputType.text,),
+              SizedBox(height: 16),
+              CustomTextInputField(hintText: "Confirm password", keyboardType: TextInputType.text),
 
-            SizedBox(height: 32),
-            CustomButton(buttonText: "Take me to home",onPressed: (){},),
-          ],
+              SizedBox(height: 32),
+              CustomButton(buttonText: "Take me to home",onPressed: (){},),
+            ],
+          ),
         ),
       ),
     );
