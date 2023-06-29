06a4fde11fb2ad44e8f7fdd53f8e40bfa34dfbef
puja335 <shahipuja335@gmail.com>
2023-06-29 19:45:28 +0545

    file rename

---
+++ lib/Screens/FinalLogPage.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../Components/Palette.dart';
+
+class finalLog extends StatelessWidget {
+  const finalLog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor:  Colors.white,
+      body: SafeArea(
+        child: Column(
+
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 0, 155, 3),
+              child: Text(
+                "Congrats!",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 0.64,
+                    color: Color(0xff000000),
+                    height: 1.1875,
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              "Let\'s get started on your journey to better\nhealth together.",
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  height: 1.25,
+                  letterSpacing: -0.16,
+                  color: Color(0xff000000),
+                ),
+              ),
+            ),
+            Container(
+              // group4FwC (522:12)
+              margin: const EdgeInsets.fromLTRB(0, 0, 0, 25),
+              width: 372,
+              height: 407,
+              child: Image.asset(
+                'assets/images/group-4.png',
+                width: 372,
+                height: 407,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(21, 0, 21, 0),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  elevation: 3,
+                  shadowColor: Pallete.primary,
+                  primary: Pallete.primary,
+                ),
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Proceed!",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      height: 1.5,
+                      letterSpacing: 0.08,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
