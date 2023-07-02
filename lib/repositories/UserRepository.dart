0b1022bdef3deb4ea27327465b6431a316d94ea0
shishir <shisirsharma22@gmail.com>
2023-07-02 20:37:59 +0545

    Added instance

--- lib/repositories/UserRepository.dart
+++ lib/repositories/UserRepository.dart
@@ -1,3 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../models/UserModel.dart';
+
 class UserRepository{
+  final instance = FirebaseFirestore.instance
+      .collection("User")
+      .withConverter<User>(
+    fromFirestore: (snapshot, _) => User.fromJson(snapshot.data()!),
+    toFirestore: (model, _) => model.toJson(),
+  );
 
 }
