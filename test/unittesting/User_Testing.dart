2d8b0e139af9afe7664a6a1d56d52365e14a62f1
shishir <shisirsharma22@gmail.com>
2023-07-03 17:59:25 +0545

    added test for addUser

--- test/unittesting/User_Testing.dart
+++ test/unittesting/User_Testing.dart
@@ -9,5 +9,15 @@ void main() {
   final UserRepository userRepository = UserRepository();
   final User genericUser=User(email: "User@mail.com", id: "1234", firstName: "John",
       lastName: "doe", gender: "male", dob: "2006");
-  
+
+  test("test to add user data", () async {
+    final response = await userRepository.addUser(genericUser);
+    final data = await response.get();
+    final actual = data
+        .data()
+        .email
+        .toString();
+    const expected = "User@mail.com";
+    expect(actual, expected);
+  });
 }
