fe5c15f5e30274d78e01d0e756816d8b90885dc6
shishir <shisirsharma22@gmail.com>
2023-07-03 18:00:50 +0545

    some minor refactor

--- test/unittesting/User_Testing.dart
+++ test/unittesting/User_Testing.dart
@@ -7,16 +7,18 @@ import 'package:medwell/services/firebase_service.dart';
 void main() {
   FirebaseService.db = FakeFirebaseFirestore();
   final UserRepository userRepository = UserRepository();
-  final User genericUser=User(email: "User@mail.com", id: "1234", firstName: "John",
-      lastName: "doe", gender: "male", dob: "2006");
+  final User genericUser = User(
+      email: "User@mail.com",
+      id: "1234",
+      firstName: "John",
+      lastName: "doe",
+      gender: "male",
+      dob: "2006");
 
   test("test to add user data", () async {
     final response = await userRepository.addUser(genericUser);
     final data = await response.get();
-    final actual = data
-        .data()
-        .email
-        .toString();
+    final actual = data.data().email.toString();
     const expected = "User@mail.com";
     expect(actual, expected);
   });
