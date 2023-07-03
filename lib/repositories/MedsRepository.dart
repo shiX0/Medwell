7a1aaf4f9b29a4c229c4130c5d5a274cca556018
aasiskrk <aasis.krk1@gmail.com>
2023-07-03 10:41:20 +0545

    Added services and added return type

--- lib/repositories/MedsRepository.dart
+++ lib/repositories/MedsRepository.dart
@@ -1,26 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:medwell/services/firebase_service.dart';
 
 import '../models/MedsModel.dart';
 
 
 class MedsRepository{
-  final instance =FirebaseFirestore.instance.collection("Meds").withConverter(fromFirestore: (snapshot, _){
+  final instance =FirebaseService.db.collection("Meds").withConverter(fromFirestore: (snapshot, _){
     return MedsModel.fromFirebaseSnapshot(snapshot);
   }, toFirestore: (MedsModel model, _)=> model.toJson());
 
-  Future<void> addMeds(MedsModel data) async{
+  Future<dynamic> addMeds(MedsModel data) async{
     try{
       final Meds = await instance.add(data);
-
+      return Meds;
     }catch(e){rethrow;}
   }
   Future<List<QueryDocumentSnapshot<MedsModel>>> getAllMeds() async{
     try{
-      final products=(await instance.get()).docs;
-      return products;
+      final Meds=(await instance.get()).docs;
+      return Meds;
     }catch(e){rethrow;}
   }
-  Future<void> deleteMeds(String id) async{
+  Future<void> deteleMeds(String id) async{
     try{
       await instance.doc(id).delete();
 
@@ -30,8 +31,8 @@ class MedsRepository{
   }
   Future<MedsModel?> getOneMeds(String id) async{
     try{
-      final product=(await instance.doc(id).get()).data();
-      return product;
+      final Meds=(await instance.doc(id).get()).data();
+      return Meds;
     }catch(e){
       rethrow;
     }
