e20bd3783ff58a4a67b02fb5e97b58977148ddb0
aasiskrk <aasis.krk1@gmail.com>
2023-07-03 10:59:44 +0545

    added test for Add meds

--- test/unittesting/Meds_Testing.dart
+++ test/unittesting/Meds_Testing.dart
@@ -1,8 +1,21 @@
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:medwell/models/MedsModel.dart';
 import 'package:medwell/repositories/MedsRepository.dart';
 import 'package:medwell/services/firebase_service.dart';
 
 void main(){
   FirebaseService.db=FakeFirebaseFirestore();
   final MedsRepository medsRepository=MedsRepository();
+  test("test to add meds", ()async{
+    final response=await medsRepository.addMeds(MedsModel(
+      medname: "synax",
+      medamount: 2,
+    ));
+    final data=await response.get();
+    print(data);
+    final actual=data.data()!.medname.toString();
+    final expected="synax";
+    expect(actual, expected);
+  });
 }
