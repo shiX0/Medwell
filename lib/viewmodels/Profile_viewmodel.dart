e07df9710fe7ac9705e542a4f04654fb7f9397e7
shishir <shisirsharma22@gmail.com>
2023-07-05 20:14:42 +0545

    Added fetch profile

--- lib/viewmodels/Profile_viewmodel.dart
+++ lib/viewmodels/Profile_viewmodel.dart
@@ -1,10 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:medwell/services/firebase_service.dart';
 
 import '../models/UserModel.dart';
+import '../repositories/UserRepository.dart';
 
 class ProfireViewModel with ChangeNotifier{
-  DocumentSnapshot<User>? _Profile;
-  DocumentSnapshot<User>? get profile => _Profile;
+  UserRepository _userRepository= UserRepository();
+  final _auth=FirebaseService.firebaseAuth.currentUser;
+  User? _Profile;
+  User? get profile => _Profile;
 
+  Future<void> fetchProfile() async{
+    try{
+      final responce = await _userRepository.getUser(_auth?.uid);
+      _Profile = responce;
+      notifyListeners();
+    }catch(e){
+      print(e);
+    }
+  }
 }
