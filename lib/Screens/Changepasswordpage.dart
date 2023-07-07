cc806de9ed0c445d6b6ec123ee83da1d54fd2450
rojalronaldo <rojalronaldo77@gmail.com>
2023-07-07 08:21:51 +0545

    Updated new code in Changepasswordpage.dart

--- lib/Screens/Changepasswordpage.dart
+++ lib/Screens/Changepasswordpage.dart
@@ -1,98 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:medwell/Components/CustomButton.dart';
+import 'package:medwell/Components/InputField.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Change Password',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ChangePasswordPage(),
-    );
-  }
-}
-
-class ChangePasswordPage extends StatefulWidget {
-  @override
-  _ChangePasswordPageState createState() => _ChangePasswordPageState();
-}
-
-class _ChangePasswordPageState extends State<ChangePasswordPage> {
-  TextEditingController _currentPasswordController = TextEditingController();
-  TextEditingController _newPasswordController = TextEditingController();
-  TextEditingController _confirmPasswordController = TextEditingController();
-
+class ChangePasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Change Password'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _currentPasswordController,
-              decoration: InputDecoration(
-                labelText: 'Current Password',
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(16),
+                child: Image.asset(
+                  'assets/images/Password_Monochromatic 1.png',
+                  height: 200,
+                ),
               ),
-              obscureText: true,
-            ),
-            TextFormField(
-              controller: _newPasswordController,
-              decoration: InputDecoration(
-                labelText: 'New Password',
+              Text(
+                'Reset Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
               ),
-              obscureText: true,
-            ),
-            TextFormField(
-              controller: _confirmPasswordController,
-              decoration: InputDecoration(
-                labelText: 'Confirm Password',
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // Add your change password logic here
-                String currentPassword = _currentPasswordController.text;
-                String newPassword = _newPasswordController.text;
-                String confirmPassword = _confirmPasswordController.text;
+              SizedBox(height: 16),
+              Text('Use a strong password.'),
+              SizedBox(height: 16),
+              CustomTextInputField(hintText: "New password", keyboardType: TextInputType.text,),
+              SizedBox(height: 16),
+              CustomTextInputField(hintText: "Confirm password", keyboardType: TextInputType.text),
 
-                if (newPassword == confirmPassword) {
-                  // Passwords match, proceed with changing password
-                  // Add your code here to change the password
-                  // For simplicity, let's just print the new password
-                  print('New password: $newPassword');
-                } else {
-                  // Passwords don't match, show an error message
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: Text('Error'),
-                      content: Text('Passwords do not match.'),
-                      actions: [
-                        TextButton(
-                          child: Text('OK'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    ),
-                  );
-                }
-              },
-              child: Text('Change Password'),
-            ),
-          ],
+              SizedBox(height: 32),
+              CustomButton(buttonText: "Take me to home",onPressed: (){},),
+            ],
+          ),
         ),
       ),
     );
