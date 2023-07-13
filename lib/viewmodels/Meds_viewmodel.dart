85477fb9ea21c799adcd150d52bf3229cff60064
aasiskrk <aasis.krk1@gmail.com>
2023-07-13 21:02:42 +0545

    Added more functions in viewmodel

--- lib/viewmodels/Meds_viewmodel.dart
+++ lib/viewmodels/Meds_viewmodel.dart
@@ -2,14 +2,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../Screens/HomePage.dart';
 import '../models/MedsModel.dart';
 import '../repositories/MedsRepository.dart';
+import '../services/firebase_service.dart';
 
 
 class MedsViewModel with ChangeNotifier {
   MedsRepository _MedsRepository = MedsRepository();
   List<MedsModel> _Meds = [];
   List<MedsModel> get Meds => _Meds;
+  MedsModel? _selectedMed;
+  MedsModel? get selectedMed => _selectedMed;
 
   Future<void> getMeds() async{
     _Meds=[];
@@ -27,6 +31,21 @@ class MedsViewModel with ChangeNotifier {
       notifyListeners();
     }
   }
+  Future<void> getOneMeds(String id) async {
+    try {
+      var response = await _MedsRepository.getOneMeds(id);
+      if (response != null) {
+        _selectedMed = response.data();
+        notifyListeners();
+      }
+    } catch (e) {
+      print(e);
+      _selectedMed = null;
+      notifyListeners();
+    }
+  }
+
+
 
 
   Future<void> addMeds(MedsModel Meds) async{
