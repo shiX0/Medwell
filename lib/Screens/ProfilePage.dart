28ff49d94389826a8ad383f1ab9d7328920dbcab
shishir <shisirsharma22@gmail.com>
2023-07-14 16:06:32 +0545

    updated user model and user vm

--- lib/Screens/ProfilePage.dart
+++ lib/Screens/ProfilePage.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:medwell/Components/CustomButton.dart';
 import 'package:medwell/Components/Palette.dart';
-import 'package:medwell/viewmodels/Profile_viewmodel.dart';
+import 'package:medwell/services/firebase_service.dart';
+import 'package:medwell/viewmodels/Auth_viewmodel.dart';
 import 'package:provider/provider.dart';
 
 import '../Components/ProfileButton.dart';
@@ -15,160 +17,187 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  late ProfileViewModel profileViewModel;
-  Future<void> fetchProfile() async{
-    try{
-      final response = await profileViewModel.fetchProfile();
-    }catch(e){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(e.toString())));
-      Navigator.of(context).pushNamed("/login");
-    }
-  }
+  late AuthViewModel _authViewModel;
 
   @override
   void initState(){
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      profileViewModel = Provider.of<ProfileViewModel>(context, listen: false);
-      fetchProfile();
+      _authViewModel = Provider.of<AuthViewModel>(context, listen: false);
+      _authViewModel.checkLogin();
     });
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
-    return Consumer<ProfileViewModel>(
-        builder: (context, profileVM, child) {
-          return Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                leading: Icon(
-                  Icons.arrow_back_rounded,
-                  size: 30,
+    return Consumer<AuthViewModel>(
+        builder: (context, authVM, child) {
+          return SafeArea(
+            child: Scaffold(
+                appBar: AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  leading: Icon(
+                    Icons.arrow_back_rounded,
+                    size: 30,
+                  ),
+                  actions: [
+                    Image.asset(
+                      "assets/icons/setting.png",
+                    )
+                  ],
                 ),
-                actions: [
-                  Image.asset(
-                    "assets/icons/setting.png",
-                  )
-                ],
-              ),
-              body: SafeArea(
-                child: Center(
-                  child: Container(
-                    child: Column(
-                      children: [
-                        Stack(children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: Color(0xffd9d9d9),
-                            ),
-                            margin: EdgeInsets.only(top: 10),
-                            height: 90,
-                            width: 90,
-                          ),
-                          Positioned(
-                            top: 60,
-                            left: 60,
-                            child: Container(
-
-                                height: 30,
-                                width: 30,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Color(0xff79d3df),
-                                ),
-                                child: Center(
-                                  child: IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      Icons.edit,
-                                      size: 20,
-                                    ),
-                                  ),
-                                )),
-                          )
-                        ]),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          child: Text("${profileVM.profile!.firstName}  ${profileVM.profile!.lastName}"
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          child: Text(profileVM.profile!.dob),
-                        ),
-                        SizedBox(
-                          height: 50,
-                        ),
-                        ProfileButtonWidget(
-                          btntitle: profileVM.profile!.email,
-                          icon: "assets/icons/Mail.png",
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        ProfileButtonWidget(
-                          btntitle: profileVM.profile!.dob,
-                          icon: "assets/icons/date.png",
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        ProfileButtonWidget(
-                          btntitle: 'Change details',
-                          icon: "assets/icons/Vector.png",
-                          width: 115,
-                          suffixIcon: Icons.arrow_forward_ios,
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        ProfileButtonWidget(
-                          btntitle: 'Change password',
-                          icon: "assets/icons/password.png",
-                          width: 90,
-                          suffixIcon: Icons.arrow_forward_ios,
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              padding: EdgeInsets.zero,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
+                body: SafeArea(
+                  child: Center(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Stack(children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Color(0xffd9d9d9),
                               ),
-                              elevation: 2,
-                              shadowColor: const Color(0xfff8f8f6),
-                              primary: const Color(0xfff8f8f6),
+                              margin: EdgeInsets.only(top: 10),
+                              height: 90,
+                              width: 90,
                             ),
-                            child: Container(
-                                height: 60,
-                                width: 350,
-                                alignment: Alignment.center,
-                                child: Text("Log out",
-                                    style: GoogleFonts.poppins(
-                                      textStyle: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        height: 1.25,
-                                        letterSpacing: -0.16,
-                                        color: Color(0xffff4747),
+                            Positioned(
+                              top: 60,
+                              left: 60,
+                              child: Container(
+
+                                  height: 30,
+                                  width: 30,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Color(0xff79d3df),
+                                  ),
+                                  child: Center(
+                                    child: IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Icons.edit,
+                                        size: 20,
                                       ),
-                                    )))),
-                        SizedBox(height: 15),
-                      ],
+                                    ),
+                                  )),
+                            )
+                          ]),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            child: Text("${authVM.loggedInUser!.firstName}  ${authVM.loggedInUser!.lastName}"
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            child: Text(authVM.loggedInUser!.dob),
+                          ),
+                          SizedBox(
+                            height: 50,
+                          ),
+                          ProfileButtonWidget(
+                            btntitle: authVM.loggedInUser!.email,
+                            icon: "assets/icons/Mail.png",
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          ProfileButtonWidget(
+                            btntitle: authVM.loggedInUser!.dob,
+                            icon: "assets/icons/date.png",
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          ProfileButtonWidget(
+                            btntitle: 'Change details',
+                            icon: "assets/icons/Vector.png",
+                            width: 115,
+                            suffixIcon: Icons.arrow_forward_ios,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          ProfileButtonWidget(
+                            btntitle: 'Change password',
+                            icon: "assets/icons/password.png",
+                            width: 90,
+                            suffixIcon: Icons.arrow_forward_ios,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                showAlertDialog(context, _authViewModel.logout());
+                              },
+                              style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                elevation: 2,
+                                shadowColor: const Color(0xfff8f8f6),
+                                primary: const Color(0xfff8f8f6),
+                              ),
+                              child: Container(
+                                  height: 60,
+                                  width: 350,
+                                  alignment: Alignment.center,
+                                  child: Text("Log out",
+                                      style: GoogleFonts.poppins(
+                                        textStyle: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          height: 1.25,
+                                          letterSpacing: -0.16,
+                                          color: Color(0xffff4747),
+                                        ),
+                                      )))),
+                          SizedBox(height: 15),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              )
+                )
+            ),
           );
         }
     );
   }
+}
+showAlertDialog(BuildContext context,Future<void> avm) {
+
+  // set up the buttons
+  Widget cancelButton = TextButton(
+    child: Text("Cancel"),
+    onPressed:  () {Navigator.of(context).pop();},
+  );
+  Widget continueButton = TextButton(
+    child: Text("logout"),
+    onPressed:  () {avm;
+    Navigator.of(context).pushReplacementNamed("/login");},
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Log out!"),
+    content: Text("Would you like to really log out?"),
+    actions: [
+      cancelButton,
+      continueButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
